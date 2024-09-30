c80b021bd079fc42019aafc4df0f307b77ff60de
Kalamar136 <karl.bridi@mail.mcgill.ca>
2024-09-30 17:56:44 -0400

    Fixed remove_all() using dummy values

--- thingifier-tests/thingifier_tests/test_categories.py
+++ thingifier-tests/thingifier_tests/test_categories.py
@@ -1,6 +1,7 @@
 import pytest
 import httpx
 from thingifier_tests.test_common import *
+import os
 
 categories = []
 #Runs once before all tests in the module
@@ -9,10 +10,14 @@ def setup_module():
     print("Setting up before all tests in the categories module")
     categories = httpx.get(url_header + "categories").json()['categories']
     remove_all()
+    print(os.getcwd())
+    print(categories)
     for category in categories:
-        httpx.post(url_header + "projects/" + category)
-    projects = httpx.post(url_header + "projects")
-    print(projects.json())
+        print("hey")
+        print(category)
+        httpx.post(url_header + "categories/" + category[id], json = category)
+    categories = httpx.post(url_header + "categories")
+    print(categories.json())
     expected : dict = {'todos': [{'id': '2', 'title': 'file paperwork', 'doneStatus': 'false', 'description': '', 'tasksof': [{'id': '1'}]}, {'id': '1', 'title': 'scan paperwork', 'doneStatus': 'false', 'description': '', 'categories': [{'id': '1'}], 'tasksof': [{'id': '1'}]}]}
     # assert response.json() == expected 
     yield
