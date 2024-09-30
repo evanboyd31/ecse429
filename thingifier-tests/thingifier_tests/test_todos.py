a64f630c83c3373c0ece19cabfcacf894cc93e9f
Charles Blancas <86479293+charlesblancas@users.noreply.github.com>
2024-09-30 14:55:20 -0400

    feat: add httpx library and example get request

--- thingifier-tests/thingifier_tests/test_todos.py
+++ thingifier-tests/thingifier_tests/test_todos.py
@@ -1,3 +1,7 @@
+import httpx
+
+url : str = "http://localhost:4567/"
+
 def test_lol():
     assert 1 == 1
 
@@ -10,3 +14,10 @@ class TestGrouping():
 
 def will_not_be_run():
     assert 1 == 2
+
+
+class TestTodos():
+    def test_get_todos(self):
+        response : httpx.Response = httpx.get(url + "todos")
+        expected : dict = {'todos': [{'id': '2', 'title': 'file paperwork', 'doneStatus': 'false', 'description': '', 'tasksof': [{'id': '1'}]}, {'id': '1', 'title': 'scan paperwork', 'doneStatus': 'false', 'description': '', 'categories': [{'id': '1'}], 'tasksof': [{'id': '1'}]}]}
+        assert response.json() == expected 
