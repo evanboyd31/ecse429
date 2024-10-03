e2d9ed44af51eae5e2a8e5a03ff870af231fd6c9
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-03 15:24:26 -0400

    Create xml tests in test_projects_id_put.py

--- thingifier-tests/thingifier_tests/projects/id/test_projects_id_put.py
+++ thingifier-tests/thingifier_tests/projects/id/test_projects_id_put.py
@@ -20,6 +20,28 @@ def test_id_put_project_with_string_boolean_json(before_each):
   assert len(response.json().get("projects")) == 1
   
   assert_project(expected=test_project, actual=response.json().get("projects")[0], check_id=True)
+
+def test_id_put_project_with_string_boolean_xml(before_each):
+  test_project = test_projects[0]
+  new_data = '''
+        <project>
+  <active type="string">true</active>
+  <description>new description</description>
+  <completed type="string">true</completed>
+  <title>new title</title>
+</project>
+    '''
+  
+  response = httpx.put(f"{projects_url}/{test_project.get("id")}", data=new_data, headers=XML_HEADERS)
+  
+  # ensure we get error message and that none of the fields are updated
+  assert response.status_code == 400
+  assert xml_to_json(response.content) == {"errorMessages": ["Failed Validation: active should be BOOLEAN, completed should be BOOLEAN"]}
+  
+  response = httpx.get(projects_url, headers=XML_HEADERS)
+  assert len(xml_to_json(response.content).get("projects")) == 1
+  
+  assert_project(expected=test_project, actual=xml_to_json(response.content).get("projects")[0], check_id=True)
   
 def test_id_put_project_with_valid_boolean_json(before_each):
   test_project = test_projects[0]
@@ -35,6 +57,23 @@ def test_id_put_project_with_valid_boolean_json(before_each):
   # ensure we update successfully and fields are updated correctly
   assert response.status_code == 200
   assert_project(expected=new_data, actual=response.json())
+  
+def test_id_put_project_with_valid_boolean_xml(before_each):
+  test_project = test_projects[0]
+  new_data = '''
+        <project>
+  <active>true</active>
+  <description>new description</description>
+  <completed>true</completed>
+  <title>new title</title>
+</project>
+    '''
+  
+  response = httpx.put(f"{projects_url}/{test_project.get("id")}", data=new_data, headers=XML_HEADERS)
+  
+  # ensure we update successfully and fields are updated correctly
+  assert response.status_code == 200
+  assert_project(expected=xml_to_json(new_data).get("project"), actual=xml_to_json(response.content).get("project"))
   
 def test_id_put_project_different_int_id_json(before_each):
   test_project = test_projects[0]
@@ -52,6 +91,24 @@ def test_id_put_project_different_int_id_json(before_each):
   assert response.status_code == 200
   assert_project(expected=new_data, actual=response.json())
   
+def test_id_put_project_different_int_id_xml(before_each):
+  test_project = test_projects[0]
+  new_data = '''
+        <project>
+        <id>200</id>
+  <active>true</active>
+  <description>new description</description>
+  <completed>true</completed>
+  <title>new title</title>
+</project>
+    '''
+  
+  response = httpx.put(f"{projects_url}/{test_project.get("id")}", data=new_data, headers=XML_HEADERS)
+  
+  # ensure we update successfully and fields are updated correctly
+  assert response.status_code == 200
+  assert_project(expected=xml_to_json(new_data).get("project"), actual=xml_to_json(response.content).get("project"))
+  
 def test_id_put_project_different_string_id_json(before_each):
   test_project = test_projects[0]
   new_data = {
@@ -67,6 +124,28 @@ def test_id_put_project_different_string_id_json(before_each):
   response = httpx.get(projects_url)
   assert len(response.json().get("projects")) == 1
   assert_project(expected=test_project, actual=response.json().get("projects")[0], check_id=True)
+
+def test_id_put_project_different_string_id_xml(before_each):
+  test_project = test_projects[0]
+  new_data = '''
+        <project>
+        <id type="string">200</id>
+  <active>true</active>
+  <description>new description</description>
+  <completed>true</completed>
+  <title>new title</title>
+</project>
+    '''
+  
+  response = httpx.put(f"{projects_url}/{test_project.get("id")}", data=new_data, headers=XML_HEADERS)
+  
+  # ensure error is thrown and existing project is not updated
+  assert response.status_code == 400
+  assert xml_to_json(response.content) == {"errorMessages":["Failed Validation: id should be ID"]}
+  
+  response = httpx.get(projects_url, headers=XML_HEADERS)
+  assert len(xml_to_json(response.content).get("projects")) == 1
+  assert_project(expected=test_project, actual=xml_to_json(response.content).get("projects")[0], check_id=True)
   
 def test_id_put_project_different_boolean_id_json(before_each):
   test_project = test_projects[0]
@@ -83,4 +162,26 @@ def test_id_put_project_different_boolean_id_json(before_each):
   response = httpx.get(projects_url)
   assert len(response.json().get("projects")) == 1
   assert_project(expected=test_project, actual=response.json().get("projects")[0], check_id=True)
+  
+def test_id_put_project_different_boolean_id_xml(before_each):
+  test_project = test_projects[0]
+  new_data = '''
+        <project>
+        <id>true</id>
+  <active>true</active>
+  <description>new description</description>
+  <completed>true</completed>
+  <title>new title</title>
+</project>
+    '''
+  
+  response = httpx.put(f"{projects_url}/{test_project.get("id")}", data=new_data, headers=XML_HEADERS)
+  
+  # ensure error is thrown and existing project is not updated
+  assert response.status_code == 400
+  assert xml_to_json(response.content) == {"errorMessages":["Failed Validation: id should be ID"]}
+  
+  response = httpx.get(projects_url, headers=XML_HEADERS)
+  assert len(xml_to_json(response.content).get("projects")) == 1
+  assert_project(expected=test_project, actual=xml_to_json(response.content).get("projects")[0], check_id=True)
   
