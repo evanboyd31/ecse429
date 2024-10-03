958b6e02db2bd62dc920d8722ab24decad9adf1d
Charles Blancas <86479293+charlesblancas@users.noreply.github.com>
2024-10-03 18:11:20 -0400

    Todos testing (#5)
    * Adding common_test file and starting test_categories
    
    * Fixed remove_all() using dummy values
    
    * Wrote basic tests for todos
    
    * some chicanery about splitting every endpoint as its own file
    
    ---------
    
    Co-authored-by: Kalamar136 <karl.bridi@mail.mcgill.ca>

---
+++ thingifier-tests/thingifier_tests/todos/conftest.py
@@ -0,0 +1,59 @@
+import httpx
+import pytest
+import thingifier_tests.test_common as common
+
+todos_url: str = "http://localhost:4567/todos"
+
+# Note that ids will change every time
+default_todos: dict = {
+    "todos": [
+        {
+            "id": "???",
+            "title": "Watch Sabrina Carpenter Concert Clips",
+            "doneStatus": "false",
+            "description": "",
+        },
+        {
+            "id": "???",
+            "title": "Buy Sabrina Carpenter Merch",
+            "doneStatus": "false",
+            "description": "",
+        },
+        {
+            "id": "???",
+            "title": "Buy Sabrina Carpenter Concert Tickets",
+            "doneStatus": "false",
+            "description": "",
+        },
+    ]
+}
+
+
+def contain_the_same_todos(todos1, todos2) -> bool:
+    todos1 = todos1["todos"]
+    todos2 = todos2["todos"]
+
+    todos1 = sorted(todos1, key=lambda todo: todo["id"])
+    todos2 = sorted(todos2, key=lambda todo: todo["id"])
+    return todos1 == todos2
+
+
+@pytest.fixture(autouse=True)
+def before_each():
+    print("hi")
+    common.remove_all()
+    titles: list[str] = [
+        "Watch Sabrina Carpenter Concert Clips",
+        "Buy Sabrina Carpenter Merch",
+        "Buy Sabrina Carpenter Concert Tickets",
+    ]
+
+    def add_to_todos(title: str):
+        res = httpx.post(todos_url, json={"title": title})
+        todo = list(
+            filter(lambda todo: todo["title"] == title, default_todos["todos"])
+        )[0]
+        todo["id"] = res.json()["id"]
+
+    for title in titles:
+        add_to_todos(title)
---
+++ thingifier-tests/thingifier_tests/todos/test_todos_post.py
@@ -0,0 +1,27 @@
+from thingifier_tests.todos.conftest import *
+
+
+class TestTodosPost:
+    def test_post_todos_should_create_a_todo(self):
+        body = {
+            "title": "Listen to Sabrina Carpenter's Latest Album",
+            "description": "Go listen to Sabrina Carpenter's newest album, 'Short n' Sweet.'",
+            "doneStatus": False,
+        }
+
+        res = httpx.post(todos_url, json=body)
+        res_json = res.json()
+
+        assert res.status_code == 201
+        assert res_json["title"] == body["title"]
+        assert res_json["description"] == body["description"]
+        assert res_json["doneStatus"] == "false"
+
+        from_api = httpx.get(todos_url + "/" + res_json["id"]).json()
+        assert from_api["todos"][0] == res_json
+
+    def test_post_todos_with_malformed_json_should_give_status_code_400(self):
+        body = '{"title": "Listen to Sabrina Carpenter\'s Latest Album"'
+        res = httpx.post(todos_url, content=body)
+
+        assert res.status_code == 400
