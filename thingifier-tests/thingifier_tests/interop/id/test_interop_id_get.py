029c319d8cbdd6a10508f0b552982194fc0eb843
Alex Turianskyj <alex.turianskyj@gmail.com>
2024-10-07 13:59:26 -0400

    Added XML tests to interop get

--- thingifier-tests/thingifier_tests/interop/id/test_interop_id_get.py
+++ thingifier-tests/thingifier_tests/interop/id/test_interop_id_get.py
@@ -1,12 +1,21 @@
 from thingifier_tests.interop.conftest import *
-
+import xmltodict
 
 class TestInteropIdGet:
     def test_get_todos_id_projects_should_get_the_associated_project(self):
         todo = default["todos"][0]
-        expected = default["projects"][0]["id"]
+        expected = default["projects"][0]["title"]
         res = httpx.get(todos_url + "/" + todo["id"] + "/tasksof")
-        actual = res.json()["projects"][0]["id"]
+        actual = res.json()["projects"][0]["title"]
+        assert res.status_code == 200
+        print(actual, expected)
+        assert expected == actual
+
+    def test_get_todos_id_projects_should_get_the_associated_project_xml(self):
+        todo = default["todos"][0]
+        expected = default["projects"][0]["title"]
+        res = httpx.get(todos_url + "/" + todo["id"] + "/tasksof", headers={"Accept": "application/xml"})
+        actual = xmltodict.parse(res.content)["projects"]["project"]["title"]
         assert res.status_code == 200
         print(actual, expected)
         assert expected == actual
@@ -20,11 +29,29 @@ class TestInteropIdGet:
         print(actual, expected)
         assert expected == actual
 
+    def test_get_todos_id_categories_should_get_the_associated_categories_xml(self):
+        todo = default["todos"][0]
+        expected = default["categories"][0]["title"]
+        res = httpx.get(todos_url + "/" + todo["id"] + "/categories", headers={"Accept": "application/xml"})
+        actual = xmltodict.parse(res.content)["categories"]["category"]["title"]
+        assert res.status_code == 200
+        print(actual, expected)
+        assert expected == actual
+
     def test_get_projects_id_todos_should_get_the_associated_todos(self):
         project = default["projects"][0]
         expected = default["todos"][0]["title"]
         res = httpx.get(projects_url + "/" + project["id"] + "/tasks")
         actual = res.json()["todos"][0]["title"]
+        assert res.status_code == 200
+        print(actual, expected)
+        assert expected == actual
+
+    def test_get_projects_id_todos_should_get_the_associated_todos_xml(self):
+        project = default["projects"][0]
+        expected = default["todos"][0]["title"]
+        res = httpx.get(projects_url + "/" + project["id"] + "/tasks", headers={"Accept": "application/xml"})
+        actual = xmltodict.parse(res.content)["todos"]["todo"]["title"]
         assert res.status_code == 200
         print(actual, expected)
         assert expected == actual
@@ -38,11 +65,29 @@ class TestInteropIdGet:
         print(actual, expected)
         assert expected == actual
     
+    def test_get_projects_id_categories_should_get_the_associated_categories_xml(self):
+        project = default["projects"][0]
+        expected = default["categories"][0]["title"]
+        res = httpx.get(projects_url + "/" + project["id"] + "/categories", headers={"Accept": "application/xml"})
+        actual = xmltodict.parse(res.content)["categories"]["category"]["title"]
+        assert res.status_code == 200
+        print(actual, expected)
+        assert expected == actual
+    
     def test_get_categories_id_todos_should_get_the_associated_todos(self):
         category = default["categories"][0]
         expected = default["todos"][0]["title"]
         res = httpx.get(categories_url + "/" + category["id"] + "/todos")
         actual = res.json()["todos"][0]["title"]
+        assert res.status_code == 200
+        print(actual, expected)
+        assert expected == actual
+    
+    def test_get_categories_id_todos_should_get_the_associated_todos_xml(self):
+        category = default["categories"][0]
+        expected = default["todos"][0]["title"]
+        res = httpx.get(categories_url + "/" + category["id"] + "/todos", headers={"Accept": "application/xml"})
+        actual = xmltodict.parse(res.content)["todos"]["todo"]["title"]
         assert res.status_code == 200
         print(actual, expected)
         assert expected == actual
@@ -55,4 +100,12 @@ class TestInteropIdGet:
         assert res.status_code == 200
         print(actual, expected)
         assert expected == actual
-
+    
+    def test_get_categories_id_projects_should_get_the_associated_projects_xml(self):
+        category = default["categories"][0]
+        expected = default["projects"][0]["title"]
+        res = httpx.get(categories_url + "/" + category["id"] + "/projects", headers={"Accept": "application/xml"})
+        actual = xmltodict.parse(res.content)["projects"]["project"]["title"]
+        assert res.status_code == 200
+        print(actual, expected)
+        assert expected == actual
