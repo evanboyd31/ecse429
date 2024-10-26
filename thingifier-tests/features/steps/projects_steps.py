13936801cd5695219dee6d4693fde442c056c92b
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-26 13:05:27 -0700

    Add step definitions for Delete Project feature file

--- thingifier-tests/features/steps/projects_steps.py
+++ thingifier-tests/features/steps/projects_steps.py
@@ -1,6 +1,4 @@
 import httpx
-import json
-import re
 
 from behave import given, when, then
 
@@ -62,6 +60,29 @@ def create_project(context, title, completed, active, description, project_id=No
 
     context.actual_project = response.json()
 
+@given('the following projects exist in the system')
+def step_given_projects_exist_in_the_system(context):
+
+    # create fake id map for mapping real project ids to the ids specified in feature files
+    context.fake_id_map = {}
+
+    for row in context.table:
+        fake_id = row.get("id")
+        title = row.get("title")
+        completed = row.get("completed")
+        active = row.get("active")
+        description = row.get("description")
+
+        create_project(context=context,
+                       title=title,
+                       completed=completed,
+                       active=active,
+                       description=description)
+        
+        real_project_id = context.response.json().get("id")
+        
+        context.fake_id_map[fake_id] = real_project_id
+
 @when('the user creates a project by specifying title {title}, completed {completed}, active {active}, and description {description}')
 def step_when_create_project(context, title, completed, active, description):
     create_project(context=context,
@@ -87,6 +108,29 @@ def step_when_create_project_with_id(context, projectId, title, completed, activ
                    active=active,
                    description=description,
                    project_id=projectId)
+
+@when('the user deletes project with id {id}')
+def step_when_user_deletes_project_with_id(context, id):
+    real_id = context.fake_id_map.get(id, id)
+    response = httpx.delete(f"{projects_url}/{real_id}")
+
+    context.response = response
+
+@when('the user sends extra query parameters {parameters} with values {values} when deleting project with id {id}')
+def step_when_the_user_deletes_project_with_id_and_extra_query_params(context, id, parameters, values):
+    real_id = context.fake_id_map.get(id, id)
+    parameters = parameters.split(",")
+    values = values.split(",")
+
+    query_param_string = "?"
+    for i, param in enumerate(parameters):
+        value = values[i]
+        current_param_string = f"{param}={value}{"&" if i != len(parameters) else ""}"
+        query_param_string += current_param_string
+
+    response = httpx.delete(f"{projects_url}/{real_id}{query_param_string}")
+
+    context.response = response
 
 
 @then('the project {project} should be created')
@@ -114,4 +158,20 @@ def step_then_response_status_code(context, statusCode):
 
 @then('the error message {errorMessage} should be raised')
 def step_then_response_error_message(context, errorMessage):
+    print(errorMessage)
+    print(context.response.json().get("errorMessages"))
     assert context.response.json().get("errorMessages") == [errorMessage]
+
+@then('the project with id {id} should not exist in the system')
+def step_then_project_with_id_should_not_exist_in_the_system(context, id):
+    # assert the original response gives 200
+    assert context.response.status_code == 200
+
+    # assert new response gives 404 and error message
+    real_id = context.fake_id_map.get("id", id)
+    response = httpx.get(f"{projects_url}/{real_id}")
+
+    assert response.status_code == 404
+    print(response.json().get("errorMessages"))
+    print()
+    assert response.json().get("errorMessages") == [f"Could not find an instance with projects/{real_id}"]
