bdb7b0a36ab51d44a5476520ed5d10cd8d15ca9d
Charles Blancas <86479293+charlesblancas@users.noreply.github.com>
2024-10-28 19:00:39 -0400

    Add given the thingifier application is running step

---
+++ thingifier-tests/features/steps/shared_steps.py
@@ -0,0 +1,16 @@
+from behave import given, when, then
+import httpx
+
+url: str = "http://localhost:4567/"
+
+
+@given("the thingifier application is running")
+def step_given_the_thingifier_application_is_running(context):
+    try:
+        response = httpx.get(url + "todos")
+        if response.status_code != 200:
+            assert False
+    except httpx.ConnectError:
+        assert False
+
+    assert False
