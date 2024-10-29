a402021425865e08e852445d8d9823418af707aa
Alex Turianskyj <alex.turianskyj@gmail.com>
2024-10-28 22:11:09 -0400

    Added shared helper function to clear system

--- thingifier-tests/features/steps/shared_steps.py
+++ thingifier-tests/features/steps/shared_steps.py
@@ -13,6 +13,25 @@ def step_given_the_thingifier_application_is_running(context):
     except httpx.ConnectError:
         assert False
 
+def remove_all():
+    response = httpx.get(url + "todos")
+    assert response.status_code == 200
+    for todo in response.json()["todos"]:
+        deleteResponse = httpx.delete(url + "todos/" + str(todo["id"]))
+        assert deleteResponse.status_code == 200
+
+    response = httpx.get(url + "categories")
+    assert response.status_code == 200
+    for category in response.json()["categories"]:
+        deleteResponse = httpx.delete(url + "categories/" + str(category["id"]))
+        assert deleteResponse.status_code == 200
+
+    response = httpx.get(url + "projects")
+    assert response.status_code == 200
+    for project in response.json()["projects"]:
+        deleteResponse = httpx.delete(url + "projects/" + str(project["id"]))
+        assert deleteResponse.status_code == 200
+
 @then('the thingifier app should return the error status "{statusCode}"')
 def step_thingifier_app_should_return_the_error_status(context, statusCode):
     assert context.response.status_code == int(statusCode)
